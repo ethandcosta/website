77cff354f6dc5f5f3e5a4e849f7014816f34f72a
Ethan D'Costa <dcosta.e@northeastern.edu>
2025-01-11 10:04:04 -0500

    test bootstrapping

--- src/components/About.js
+++ src/components/About.js
@@ -1,24 +1,35 @@
 import SpaceBackground from "./SpaceBackground";
-import me from  "../imgs/me.JPG"
-import "../About.css"
-/* retest*/
+import me from "../imgs/me.JPG";
+import "../About.css";
+
+/* retest */
 const About = () => (
-    <section>
-      <SpaceBackground />
-      <h2>About Me</h2>
-      <div className="profile">
-        <img src={me} alt="profile pic" width="300" height="400"/>
-        <p>
-          I'm Ethan D'Costa, a recent graduate from Northeastern University, where I 
+  <section className="container py-5">
+    <SpaceBackground />
+    <h2 className="text-center mb-4">About Me</h2>
+
+
+    <div className="my-profile row align-items-center"> 
+      <div className="col-12 col-md-4 mb-4 mb-md-0">
+        <img
+          src={me}
+          alt="profile pic"
+          className="img-fluid rounded-circle shadow"
+          width="500"
+          height="300"
+        />
+      </div>
+      <div className="col-12 col-md-8">
+        <p className="lead">
+          I'm Ethan D'Costa, a recent graduate from Northeastern University, where I
           majored in computer science and physics and minored in math. I'm passionate about
           leveraging my knowledge in software to make positive, tangible impacts on society through
           programs that can provide unique insights about data and can serve
           as a medium for creative expression.
         </p>
       </div>
-      
-    </section>
-  );
-  
-  export default About;
-  
+    </div>
+  </section>
+);
+
+export default About;
