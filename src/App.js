56383a8f9a508ab6c81989a7f5f319b3c13bf7f1
Ethan D'Costa <dcosta.e@northeastern.edu>
2025-01-10 19:47:52 -0500

    rid of skills, fixed about

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
-import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
@@ -15,7 +14,6 @@ const App = () => (
     <Header />
     <Routes>
       <Route path="/website" element={<About />} />
-      <Route path="/skills" element={<Skills />} />
       <Route path="/projects" element={<Projects />} />
       <Route path="/experience" element={<Experience />} />
       <Route path="/contact" element={<Contact />} />
--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import SpaceBackground from "./SpaceBackground";
-
+import me from  "../imgs/me.JPG"
+import "../About.css"
 /* retest*/
 const About = () => (
     <section>
-    <SpaceBackground />
+      <SpaceBackground />
       <h2>About Me</h2>
-      <p className="contact-info">
-        I'm Ethan D'Costa, a computer science and physics student passionate about 
-        creating accessible and innovative software solutions.
-      </p>
+      <div className="profile">
+        <img src={me} alt="profile pic" width="300" height="400"/>
+        <p>
+          I'm Ethan D'Costa, a computer science and physics student passionate about 
+          creating accessible and innovative software solutions.
+        </p>
+      </div>
+      
     </section>
   );
   
