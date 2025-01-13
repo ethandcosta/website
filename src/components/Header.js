5075773cd7c2dba41df87ca6440e0b2cb1c0b798
Ethan D'Costa <dcosta.e@northeastern.edu>
2025-01-12 22:52:14 -0500

    minor patch

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => (
   <header>
     <h1>Ethan D'Costa</h1>
     <nav>
-      <Link to="/website">About</Link>
+      <Link to="/">About</Link>
       <Link to="/projects">Projects</Link>
       <Link to="/experience">Experience</Link>
       <Link to="/contact">Contact</Link>
