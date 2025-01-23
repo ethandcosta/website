072c9f9b63dd616e32cfffa0830aa3051629f7ec
Ethan D'Costa <dcosta.e@northeastern.edu>
2025-01-23 13:50:30 -0500

    added section for northeastern

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,6 +2,7 @@ import SpaceBackground from "./SpaceBackground";
 import "../Experience.css"
 import mideLogo from "../imgs/mide.png"
 import stateStreetLogo from "../imgs/state_street.png"
+import northeasternLogo from "../imgs/northeastern.png"
 
 const ExperienceCard = ({ position, company, img, dates, description }) => {
   return (
@@ -41,6 +42,13 @@ const Experience = () => (
       img = {stateStreetLogo}
       dates = "Jan 2023 - Jul 2023"
       description = "At State Street, I developed file-injection testing programs in Azure Chaos Studio to enhance server resiliency against security threats managing $4T in financial transactions and built a Python adapter to deserialize JSON chatbot requests into a Microsoft Teams-integrated dashboard."
+      />
+      <ExperienceCard 
+      position= "Teaching Assistant"
+      company = "Northeastern University"
+      img= {northeasternLogo}
+      dates = "Aug 2022 - Dec 2024"
+      description = "I was a teaching assistant for two classes: Object-Oriented Design and Professional Development for Co-Op. With OOD, I ran a lab section of 20 students where I reviewed design patterns and philosophies like SOLID principles. In the co-op class, I taught students how to sharpen their resumes, deliver strong interviews, and effectively use their network to get jobs."
       />
       </div>
     </section>
